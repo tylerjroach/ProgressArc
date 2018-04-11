0045d07d11d5d0a903a28bcd7682a7472dfdc41c
Tyler Roach <tylerjroach@gmail.com>
2018-04-11 11:35:13 -0400

    added listener the detect optimal rectangle bounds for any content that developer may want to place in center.

--- demo/src/main/java/com/tylerjroach/progressarcdemo/MainActivity.java
+++ demo/src/main/java/com/tylerjroach/progressarcdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.tylerjroach.progressarcdemo;
 
+import android.graphics.Rect;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SeekBar;
 
 import com.tylerjroach.progressarc.ProgressArc;
@@ -9,7 +12,9 @@ import com.tylerjroach.progressarc.ProgressArc;
 public class MainActivity extends AppCompatActivity {
 
     ProgressArc progressArc;
-    SeekBar seekBar;
+    SeekBar progressSeekBar;
+    SeekBar sweepAngleSeekBar;
+    View innerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +22,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         progressArc = findViewById(R.id.progressArc);
-        seekBar = findViewById(R.id.seekBar);
+        innerView = findViewById(R.id.innerView);
 
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        progressSeekBar = findViewById(R.id.progressSeekBar);
+        progressSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressArc.setProgress(progress);
@@ -36,5 +42,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        sweepAngleSeekBar = findViewById(R.id.sweepAngleSeekBar);
+        sweepAngleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressArc.setSweepAngle((int) (180 + ((progress / 100f) * 180)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        innerView.post(new Runnable() {
+            @Override
+            public void run() {
+                progressArc.setOnInnerRectangleSizeChangedListener(new ProgressArc.OnInnerRectangleBoundsChangedListener() {
+                    @Override
+                    public void onInnerRectangleSizeChanged(Rect rect) {
+                        ((ConstraintLayout.LayoutParams)innerView.getLayoutParams()).width = rect.width();
+                        ((ConstraintLayout.LayoutParams)innerView.getLayoutParams()).height = rect.height();
+                        ((ConstraintLayout.LayoutParams)innerView.getLayoutParams()).setMargins((progressArc.getWidth() - rect.width()) / 2, (progressArc.getWidth() - rect.width()) / 2, 0, 0);
+                        innerView.requestLayout();
+                    }
+                });
+            }
+        });
+
+        progressSeekBar.setProgress(50);
+        sweepAngleSeekBar.setProgress((int) (progressArc.getSweepAngle() / 360f * 100));
     }
 }
--- library/src/main/java/com/tylerjroach/progressarc/ProgressArc.java
+++ library/src/main/java/com/tylerjroach/progressarc/ProgressArc.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.support.annotation.IntRange;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -16,17 +18,21 @@ import static android.graphics.Paint.Style.STROKE;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class ProgressArc extends View {
-
     private static final int MAX_PROGRESS = 100;
 
-    private RectF arcBounds = new RectF();
     private Paint paint;
-    private int strokeWidth;
+    private RectF arcBounds = new RectF();
+
+    private OnInnerRectangleBoundsChangedListener onInnerRectangleSizeChangedListener;
+
+    private int arcStrokeWidth;
+    private int arcBackgroundColor;
+    private int arcStrokeColor;
+    private int startAngle;
     private int sweepAngle;
     private int progressSweepAngle;
-    private int startAngle;
-    private int strokeColor;
-    private int backgroundColor;
+    private int innerRectangleWidth, innerRectangleHeight;
+
     private float progress;
 
     public ProgressArc(Context context) {
@@ -52,45 +58,64 @@ public class ProgressArc extends View {
 
     private void init(Context context, AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ProgressArc);
-        strokeWidth = typedArray.getDimensionPixelSize(R.styleable.ProgressArc_stroke_width, getResources().getDimensionPixelSize(R.dimen.default_stroke_width));
-        backgroundColor = typedArray.getColor(R.styleable.ProgressArc_stroke_inactive_color, getResources().getColor(R.color.progressBarInactive));
-        strokeColor = typedArray.getColor(R.styleable.ProgressArc_stroke_active_color, getResources().getColor(R.color.progressBarActive));
+        arcStrokeWidth = typedArray.getDimensionPixelSize(R.styleable.ProgressArc_stroke_width, getResources().getDimensionPixelSize(R.dimen.default_stroke_width));
+        arcBackgroundColor = typedArray.getColor(R.styleable.ProgressArc_stroke_inactive_color, getResources().getColor(R.color.progressBarInactive));
+        arcStrokeColor = typedArray.getColor(R.styleable.ProgressArc_stroke_active_color, getResources().getColor(R.color.progressBarActive));
         sweepAngle = typedArray.getInteger(R.styleable.ProgressArc_sweep_angle, getResources().getInteger(R.integer.default_sweep_angle));
         typedArray.recycle();
-
-        startAngle = 90 + ((360 - sweepAngle) / 2);
 
         paint = new Paint();
         paint.setAntiAlias(true);
         paint.setStyle(STROKE);
         paint.setStrokeCap(ROUND);
-        paint.setStrokeWidth(strokeWidth);
+        paint.setStrokeWidth(arcStrokeWidth);
+
+        updateStartAngle();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         boolean isWrapContent = getLayoutParams().height == WRAP_CONTENT;
         int width = MeasureSpec.getSize(widthMeasureSpec);
-        int height = MeasureSpec.getSize(isWrapContent ? widthMeasureSpec, heightMeasureSpec);
-        arcBounds.set(strokeWidth / 2, strokeWidth / 2, width - (strokeWidth / 2), height - strokeWidth / 2);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int arcHeight = MeasureSpec.getSize(isWrapContent ? widthMeasureSpec : heightMeasureSpec);
+        arcBounds.set(arcStrokeWidth / 2, arcStrokeWidth / 2, width - (arcStrokeWidth / 2), arcHeight - arcStrokeWidth / 2);
 
         if (getLayoutParams().height == ViewGroup.LayoutParams.WRAP_CONTENT) {
             float radius = arcBounds.width() / 2;
             double layoutHeight = radius * (1 - Math.cos(Math.toRadians(sweepAngle) / 2));
-            double strokeArcOffset = strokeWidth - ((sweepAngle - 180) / 180);
-            setMeasuredDimension(width, (int) Math.ceil(layoutHeight + strokeArcOffset));
+            double strokeArcOffset = arcStrokeWidth - ((sweepAngle - 180) / 180);
+            height = (int) Math.ceil(layoutHeight + strokeArcOffset);
+            setMeasuredDimension(width, height);
         } else {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         }
+
+        int innerCircleDiameter = width - (arcStrokeWidth * 2);
+        int squareSideLength = (int) (Math.ceil((innerCircleDiameter * Math.sqrt(2)) / 2));
+        updateInnerRectangle(squareSideLength, height < squareSideLength ? height : squareSideLength);
     }
 
     @Override protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        paint.setColor(backgroundColor);
+        paint.setColor(arcBackgroundColor);
         canvas.drawArc(arcBounds, startAngle, sweepAngle, false, paint);
-        paint.setColor(strokeColor);
+        paint.setColor(arcStrokeColor);
         canvas.drawArc(arcBounds, startAngle, progressSweepAngle, false, paint);
+    }
 
+    private void updateInnerRectangle(int width, int height) {
+        if (innerRectangleWidth != width || innerRectangleHeight != height) {
+            innerRectangleWidth = width;
+            innerRectangleHeight = height;
+            if (onInnerRectangleSizeChangedListener != null) {
+                onInnerRectangleSizeChangedListener.onInnerRectangleSizeChanged(new Rect(0, 0, width, height));
+            }
+        }
+    }
+
+    private void updateStartAngle() {
+        startAngle = 90 + ((360 - sweepAngle) / 2);
     }
 
     public float getProgress() {
@@ -99,7 +124,34 @@ public class ProgressArc extends View {
 
     public void setProgress(float progress) {
         this.progress = progress;
-        progressSweepAngle = (int) (progress / MAX_PROGRESS * sweepAngle);
+        progressSweepAngle = (int) Math.ceil((progress / MAX_PROGRESS * sweepAngle));
         invalidate();
+    }
+
+    public int getSweepAngle() {
+        return sweepAngle;
+    }
+
+    public void setSweepAngle(@IntRange(from = 180, to = 360) int sweepAngle) {
+        this.sweepAngle = sweepAngle;
+        updateStartAngle();
+        setProgress(progress);
+        invalidate();
+        requestLayout();
+    }
+
+    public OnInnerRectangleBoundsChangedListener getOnInnerRectangleSizeChangedListener() {
+        return onInnerRectangleSizeChangedListener;
+    }
+
+    public void setOnInnerRectangleSizeChangedListener(final OnInnerRectangleBoundsChangedListener onInnerRectangleSizeChangedListener) {
+        this.onInnerRectangleSizeChangedListener = onInnerRectangleSizeChangedListener;
+        if (innerRectangleWidth != 0 && innerRectangleHeight != 0) {
+            onInnerRectangleSizeChangedListener.onInnerRectangleSizeChanged(new Rect(0, 0, innerRectangleWidth, innerRectangleHeight));
+        }
+    }
+
+    public interface OnInnerRectangleBoundsChangedListener {
+        void onInnerRectangleSizeChanged(Rect rect);
     }
 }
